bcc08e8496c5ba6df946e61f2e9ec474f83d710b
anton popov <lankastersky@gmail.com>
2017-12-28 10:09:37 -0800

    Fragment challenge and Journal fragment challenge layouts were separated.

--- zen/app/src/main/java/com/hcyclone/zen/view/JournalChallengeFragment.java
+++ zen/app/src/main/java/com/hcyclone/zen/view/JournalChallengeFragment.java
@@ -32,8 +32,9 @@ public class JournalChallengeFragment extends ChallengeFragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_challenge, container, false);
+
+    View view =
+        inflater.inflate(R.layout.fragment_journal_challenge, container, false);
 
     getActivity().setTitle(getString(R.string.fragment_challenge_journal_entry));
 
